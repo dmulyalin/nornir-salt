65e45cc9d6f7979bd40c90babbdac38c0007e6e5
Denis <d.mulyalin@gmail.com>
2021-03-15 22:48:14 +1100

    added docs for nr_test and netmiko_send_commands task plugins

--- nornir_salt/plugins/tasks/netmiko_send_commands.py
+++ nornir_salt/plugins/tasks/netmiko_send_commands.py
@@ -1,3 +1,38 @@
+"""
+netmiko_send_commands
+#####################
+
+This test plugin uses ``nornir-netmiko`` ``netmiko_send_command`` task 
+to send multiple commands to devices.
+
+Dependencies:
+
+* `nornir-netmiko module <https://pypi.org/project/nornir-netmiko/>`_ required
+
+netmiko_send_commands sample usage
+==================================
+
+Code to invoke ``netmiko_send_commands`` task::
+
+    from nornir_salt import netmiko_send_commands
+    
+    output = nr.run(
+        task=netmiko_send_commands,
+        commands=["show run", "show clock"],
+        netmiko_kwargs={}
+    )
+	
+netmiko_send_commands returns
+=============================
+
+Returns Nornir results object with individual tasks names set
+equal to commands sent to device.
+
+netmiko_send_commands reference
+===============================
+
+.. autofunction:: nornir_salt.plugins.tasks.netmiko_send_commands.netmiko_send_commands
+"""
 from nornir.core.task import Result, Task
 from nornir_netmiko.tasks import netmiko_send_command
 
@@ -11,8 +46,9 @@ def netmiko_send_commands(task, commands, **kwargs):
     ``nornir_netmiko.tasks.netmiko_send_command`` plugin
 
     :param kwargs: might contain ``netmiko_kwargs`` argument dictionary
-         for ``nornir_netmiko.tasks.netmiko_send_command`` method
-    :param config: (list) commands list to send to device(s)
+        with parameters for ``nornir_netmiko.tasks.netmiko_send_command`` 
+        method
+    :param commands: (list) commands to send to device
     :return result: Nornir result object with task execution results
     """
     for command in commands:
--- nornir_salt/plugins/tasks/nr_test.py
+++ nornir_salt/plugins/tasks/nr_test.py
@@ -1,21 +1,48 @@
+"""
+nr_test
+#######
+
+Function to test Nornir operation and produce valid results
+without the need to interact with devices directly.
+
+nr_test sample usage
+====================
+
+Code to invoke ``nr_test`` task::
+
+    from nornir_salt import nr_test
+    
+    output = nr.run(task=nr_test, abc=123)
+    
+nr_test returns
+=============================
+
+Returns Nornir results object with task results populated
+in accordance with arguments supplied to task call.
+
+nr_test reference
+=================
+
+.. autofunction:: nornir_salt.plugins.tasks.nr_test.nr_test
+"""
 from nornir.core.task import Result
 
 def nr_test(task, ret_data_per_host={}, ret_data="__undefined_value__", **kwargs):
     """
     Dummy task that echoes data passed to it. Useful to debug and
-    verify Nornir object operation
+    verification of Nornir object operation.
 
     :param ret_data_per_host: (dict) Dictionary keyed by host name with
         values to include in results
-    :param ret_data: Any data to include in results
-    :param kwargs: Any key-value pair to include in results
+    :param ret_data: Any data to include in results, same for each host
+    :param kwargs: Any key-value pair to include in results, same for each host
     :return result: ``ret_data`` or ``**kwargs`` passed to the task
     
     Order of preference of return data:
     
-    1. ``ret_data_per_host`` included in results 
-    2. ``ret_data`` included in results 
-    3. ``**kwargs`` included in results 
+    1. If ``ret_data_per_host`` present, it is used to form results 
+    2. If ``ret_data`` present, it is included in results 
+    3. If ``**kwargs`` supplied, they are included in results
     """
     if ret_data_per_host:
         return Result(host=task.host, result=ret_data_per_host.get(task.host.name, None))
